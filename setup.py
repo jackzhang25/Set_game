18acd77f66fad666c6b2adea12bc0198dfe23deb
jackzhang25 <jackzhang25@live.com>
2019-07-27 18:31:03 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import random 
+colors = ["r","g","b"]
+shading = ["full", "shaded", "empty"]
+shape = ["diamond", "oval", "rectangle"]
+class setGame:
+    def __init__(self):
+        self.deck = None
+        self.generateDeck()
+        self.shuffle()
+    def __repr__(self):
+        return self.__str__()
+    def __str__(self):
+        repr_str = ''
+        for card in self.deck:
+            repr_str += '\n' + card.__str__()
+        return repr_str
+    def generateDeck(self):
+        deck = []
+        for i in range(1,4):
+            num = i 
+            for col in colors:
+                color = col
+                for shade in shading:
+                    shaded = shade
+                    for shapes in shape:
+                        deck.append([num, color, shaded, shapes])
+        self.deck = deck
+    def shuffle(self):
+        random.shuffle(self.deck)
+if __name__ == '__main__':
+    deck = setGame()
+    deck.shuffle()
+    print(deck)
